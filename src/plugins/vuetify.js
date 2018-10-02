d30d674f5e296a4dcf556c60bdb0936577ae8577
Bryan <bryan.pedroza@ymail.com>
2018-10-01 17:58:59 -0700

    favicon and webmanist updates

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,10 +14,14 @@ import {
   VForm,
   VTextField,
   VAlert
-} from 'vuetify'
-import 'vuetify/src/stylus/app.styl'
+} from 'vuetify';
+import 'vuetify/src/stylus/app.styl';
+import colors from 'vuetify/es5/util/colors';
 
 Vue.use(Vuetify, {
+  theme: {
+    primary: colors.blue.lighten2
+  },
   components: {
     VApp,
     VNavigationDrawer,
@@ -32,5 +36,5 @@ Vue.use(Vuetify, {
     VForm,
     VTextField,
     VAlert
-  },
-})
+  }
+});
